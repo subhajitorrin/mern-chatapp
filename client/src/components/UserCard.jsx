05efad89e9e11288ab98862166d5f9d50ec7496f
Subhajit Ghosh <subhajitghosh343@gmail.com>
2024-08-23 16:09:30 +0530

    frontend in progress

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoDotFill } from "react-icons/go";
 
 function UserCard() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="hover:bg-[#ffffff1e] cursor-pointer px-[1rem] border-b border-[#ffffff24] flex justify-between items-center py-[14px]">
       <div className="flex items-center gap-[10px]">
         <span className="relative">
-          <GoDotFill className="text-[#00ff00] text-[18px] absolute right-[-3px] top-[-3px]" />
+          {isActive && (
+            <GoDotFill className="text-[#00ff00] text-[18px] absolute right-[-3px] top-[-3px]" />
+          )}
           <img
             src=""
             className="h-[40px] w-[40px] rounded-[100%] border border-white"
