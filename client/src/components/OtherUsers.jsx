ec8804656c36640e5a6cffd6c85acd842edcd415
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-08 01:41:24 +0530

    both side new chat

--- client/src/components/OtherUsers.jsx
+++ client/src/components/OtherUsers.jsx
@@ -4,7 +4,7 @@ import { useAuthStore } from "../store/AuthStore.js";
 
 function OtherUsers() {
   const [connectionList, setConnectionList] = useState([]);
-  const { connections, getConnections, user } = useAuthStore();
+  const { connections, getConnections, user,socket,checkAndUpdateConnection } = useAuthStore();
   useEffect(() => {
     if (user) {
       getConnections();
@@ -14,13 +14,32 @@ function OtherUsers() {
   useEffect(() => {
     if (connections.length > 0) {
       setConnectionList(connections);
+    } else {
+      setConnectionList([]);
     }
   }, [connections]);
+
+
+  useEffect(() => {
+    if (!socket) return;
+    async function handleReceiveSocketMsg(data) {
+      checkAndUpdateConnection(data, data.user, true);
+      // setPendingMsg(data);
+    }
+
+    socket.on("receiveSocketMsg", handleReceiveSocketMsg);
+
+    return () => {
+      socket.off("receiveSocketMsg", handleReceiveSocketMsg);
+    };
+  }, [socket]);
 
   return (
     <div className="scrollNone flex flex-col h-[80%] overflow-y-auto">
       {connectionList.map((item, index) => {
-        return <UserCard key={index} user={item.user} lastMsg = {item.updatedAt} />;
+        return (
+          <UserCard key={index} user={item.user} lastMsg={item.updatedAt} />
+        );
       })}
     </div>
   );
